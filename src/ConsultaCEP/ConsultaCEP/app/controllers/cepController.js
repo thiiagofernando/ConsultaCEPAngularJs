f683b6233ed5394edb8fd79bd8b6b6b2cb349d2a
thiiagofernando <thiago.fernando@msn.com>
2019-12-02 01:00:52 -0400

    demo finalizada

---
+++ src/ConsultaCEP/ConsultaCEP/app/controllers/cepController.js
@@ -0,0 +1,12 @@
+﻿app.controller('cepController', function($scope,$http) {
+    $scope.consultacep = function (cepdigitado) {
+        $http({
+            method: 'get',
+            url: 'https://viacep.com.br/ws/' + cepdigitado + '/json/'
+        }).then(function (response) {
+            $scope.resultado = [response.data];
+        }, function (error) {
+            console.log(error, 'deu erro');
+        });
+    };
+});
